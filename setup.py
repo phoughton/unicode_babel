b0801a4a38d2ea2ed436420d5a611563ffb47fd2
Peter Houghton <pete@investigatingsoftware.co.uk>
2019-11-29 14:23:11 +0000

    Include files required for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="unicode_babel-phoughton",
+    version="0.0.1",
+    author="Peter Houghton",
+    author_email="pete@investigatingsoftware.co.uk",
+    description="A tool for generating random characters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phoughton/unicode_babel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
