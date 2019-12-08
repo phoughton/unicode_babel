1298250ed4904b0d915124172c8a6f643ca5d966
Peter Houghton <pete@investigatingsoftware.co.uk>
2019-12-08 15:25:14 +0000

    Add tests for plane range integrity and include fixes for issues found.

--- tests/tests_1.py
+++ tests/tests_1.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
-sys.path.append(".")
-sys.path.append("..")
-sys.path.append("../unicode_babel")
+# sys.path.append(".")
+# sys.path.append("..")
+# sys.path.append("../unicode_babel")
 from unicode_babel import tools, filters
 
 
@@ -17,6 +17,28 @@ class TestUnicodeGenerator(unittest.TestCase):
     def test_get_codepoint_no_filter_plane_1(self):
         genny = tools.CodePointGenerator([1])
         self.assertEqual(1, len(genny.get_random_codepoint()), "vanilla get codepoint may be unnamed")
+
+    def test_get_codepoint_no_filter_plane_0_check_in_range(self):
+        genny = tools.CodePointGenerator([0])
+
+        for tries in range(100):
+            gen_char = genny.get_random_codepoint()
+            self.assertTrue(ord(gen_char) <= 0xFFFF, f"The codepoint was: {ord(gen_char)} in try: {tries}")
+            self.assertTrue(ord(gen_char) >= 0x0000, f"The codepoint was: {ord(gen_char)} in try: {tries}")
+
+    def test_get_codepoint_no_filter_plane_1_check_in_range(self):
+        genny = tools.CodePointGenerator([1])
+        gen_char = genny.get_random_codepoint()
+        for tries in range(100):
+            self.assertTrue(ord(gen_char) <= 0x1FFFF, f"The codepoint was: {hex(ord(gen_char))} in try: {tries}")
+            self.assertTrue(ord(gen_char) >= 0x10000, f"The codepoint was: {hex(ord(gen_char))} in try: {tries}")
+
+    def test_get_codepoint_no_filter_planes_1_and_2_check_in_range(self):
+        genny = tools.CodePointGenerator([0, 1])
+        gen_char = genny.get_random_codepoint()
+        for tries in range(100):
+            self.assertTrue(ord(gen_char) <= 0x1FFFF, f"The codepoint was: {hex(ord(gen_char))} in try: {tries}")
+            self.assertTrue(ord(gen_char) >= 0x0000, f"The codepoint was: {hex(ord(gen_char))} in try: {tries}")
 
     def test_get_codepoint_filter_out_unnamed_plane_0(self):
         genny = tools.CodePointGenerator([0])
